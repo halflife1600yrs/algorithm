a0c0fdc36abf5974bfde07050a1cf0a00e6db523
Z131_ <zbl991027@gmail.com>
2019-01-25 10:56:00 +0800

    edit gitignore

--- wannafly/day1/J.cpp
+++ wannafly/day1/J.cpp
@@ -2,26 +2,42 @@
 
 using namespace std;
 
-typedef pair<int, int> pII;
-int item[1000][1000];
-pII head[1000];
+#define DEBUG
+#define debug1(x) std::cout << #x " = " << x << std::endl
+#define debug2(x, y) std::cout << #x " = " << x << " ," #y " = " << y << std::endl
+#define disp(arry, fr, to) \
+    std::cout << #arry " : "; \
+    for(int i = fr; i < to; i++) std::cout << arry[i] << " "; \
+    std::cout << std::endl;
 
-bool cmp(pII a, pII b) { return a.first > b.first; }
+
+const int MAXN=1005;
+typedef pair<int,int> pII;
+pII ctzn[MAXN];
+int items[MAXN][MAXN];
+
+bool cmp(pII a,pII b) { return a.second>b.second; }
 
 int main()
 {
-    int n, m;
-    while (~scanf("%d %d", &n, &m))
+    int n,m;
+    while(~scanf("%d %d",&n,&m))
     {
-        int a, b;
-        memset(head, 0, sizeof(head));
-        for (int i = 0; i < m; ++i)
+        for(int i=0;i<n;++i)
+            ctzn[i]=pII(i,0);
+        int a,b;
+        for(int i=0;i<m;++i)
         {
-            scanf("%d %d", &a, &b);
-            item[a][head[a].first++] = b, head[a].second = i;
+            scanf("%d %d",&a,&b);
+            items[b][ctzn[b].second++]=a;
         }
-        for (int i = 0; i < a; ++i)
-            sort(item[i][0], item[i][head[i].first]);
-        sort(head, head + n, cmp);
+        for(int i=0;i<n;++i)
+        {
+            sort(items[i],items[i]+ctzn[i].second);
+            disp(items[i],0,ctzn[i].second)
+        }
+        sort(ctzn,ctzn+n,cmp);
+
     }
+    return 0;
 }
